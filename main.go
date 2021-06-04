ef849307195beb890675ffe8c00c4e97a9f10530
sanardiazam <rubypromise@gmx.com>
2021-06-04 12:43:37 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/pluralsight/webservice/models"
+)
+
+func main() {
+
+	arr := [3]string{}
+	arr[0] = "apple"
+	arr[1] = "plum"
+	arr[2] = "orange"
+	fmt.Println(arr)
+
+	b := contains(arr, "plum")
+	fmt.Println(b)
+
+	b = contains(arr, "grape")
+	fmt.Println(b)
+
+	u := models.User{
+		ID:        2,
+		FirstName: "Marzia",
+		LastName:  "Azam",
+	}
+	fmt.Println(u)
+}
+
+func GetUsers() []*User {
+	return users
+}
+
+func contains(arr [3]string, str string) bool {
+	for _, a := range arr {
+		if a == str {
+			return true
+		}
+	}
+	return false
+}
